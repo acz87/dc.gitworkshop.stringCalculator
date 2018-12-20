842c57f0b19ba269e1ee8c8467a83bd6eef80789
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-12-20 13:29:00 +0100

    Add project files.

---
+++ dc.gitworkshop.stringCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dc.gitworkshop.stringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
